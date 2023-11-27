4a9a1d239ef44f9849be3dd7e3a9029f216f847c
les-knick <lea.stocker@knickdesign.de>
2023-11-27 11:41:01 +0100

    open zip

---
+++ README.md
@@ -0,0 +1,1 @@
+# ais-dka-custom
---
+++ assets/js/scripts.js
@@ -0,0 +1,52 @@
+function toggleMobileMenu() {
+    var mobileMenu = document.getElementById('primary-nav');
+    mobileMenu.classList.toggle('mobile-nav--active');
+}
+
+window.addEventListener('load', function() {
+    var triggerButton = document.getElementById('search-string-from-home');
+    if(triggerButton){
+    triggerButton.addEventListener('click', function() {
+        storeSearchString();
+    });
+}
+});
+
+function storeSearchString() {
+        var storedSearchString = document.getElementById("searchinput");
+        localStorage.setItem("searchString", storedSearchString.value);
+        window.location.href = "/archivalien-suche";
+}
+
+window.addEventListener('load', function() {
+    getSearchString();
+});
+
+function getSearchString() {
+        var searchInputField = document.getElementById("edit-portal-view-anonymus-rendered-item");
+        var searchString = localStorage.getItem("searchString");
+
+        if (searchString != "alreadySearchedThis" && searchString) {
+            searchInputField.value = searchString;
+
+            localStorage.setItem("searchString", "alreadySearchedThis");
+
+            var submitBtn = document.getElementById('edit-submit-archivalien-suche-frei-ii');
+            submitBtn.click();
+        } else if (!searchString) {
+            searchInputField.value = "";
+        }
+}
+
+window.addEventListener('load', function() {
+    var filterButton = document.querySelector('.sidebar--content__open');
+    if(filterButton){
+        var beforeFilter = document.querySelector('.view-filters');
+        beforeFilter.appendChild(filterButton);
+    }
+});
+
+function toggleSidebar() {
+    var filter = document.querySelector('.sidebar--content');
+    filter.classList.toggle('filter--active');
+}
